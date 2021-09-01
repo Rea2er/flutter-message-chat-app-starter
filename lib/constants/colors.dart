b66d3b3229a2bef251e0f6d2a51856ab251cfa9b
Ruize Nie <rz376775@dal.ca>
2021-09-01 00:18:56 -0300

    upload the assets folder

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryLight = Color(0xFFEDEEF7);
+const Color kPrimary = Color(0xFF5B60BA);
+const Color kPrimaryDark = Color(0xFF000036);
+const Color kGreyLight = Color(0xFFB4B7BF);
